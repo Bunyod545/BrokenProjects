ca7772b60596783e610a2bef0ce4f9578dbeae60
HP <HP@DESKTOP-GDLLM62>
2021-02-08 20:40:00 +0500

    Decorator pattern problem added

---
+++ Patterns/DecoratorPatternApi/Services/Notifications/Interfaces/INotifier.cs
@@ -0,0 +1,14 @@
+﻿namespace DecoratorPatternApi.Services.Notifications.Interfaces
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface INotifier
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="message"></param>
+        void Send(string message);
+    }
+}
